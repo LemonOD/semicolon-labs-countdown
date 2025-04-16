f3f1abb49587482a08179783e6be9427ac0d6f94
LemonOD <t.lemon@semicolon.africa>
2025-04-16 11:55:06 +0100

    adjusted the footer info

--- src/components/layouts/footer.tsx
+++ src/components/layouts/footer.tsx
@@ -4,14 +4,14 @@ export function Footer() {
   return (
     <footer className="border-t bg-muted/40">
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
-        <div className="flex flex-col gap-2">
+        <div className="flex flex-col gap-0.5">
           <div className="flex items-center gap-2">
             <SemicolonLogo />
             <span className="text-sm font-semibold">Labs by Semicolon</span>
           </div>
           <div>
-            <p className="text-center text-sm text-muted-foreground font-semibold">Built to Solve. Structured for Scale.
-              Designed for the Future.</p>
+            <small className="text-center md:text-sm text-muted-foreground md:font-semibold">Built to Solve. Structured for Scale.
+              Designed for the Future.</small>
           </div>
 
         </div>

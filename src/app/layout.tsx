a554426a5e30122872d5b47b69a820b2f2e79baf
LemonOD <t.lemon@semicolon.africa>
2025-04-17 16:06:03 +0100

    added favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,23 @@
 import type React from "react"
 import "@/src/app/globals.css"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/src/components/layouts/theme-provider"
+import {Inter} from "next/font/google"
+import {ThemeProvider} from "@/src/components/layouts/theme-provider"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata = {
-  title: "Semicolon Labs -Coming Soon",
-  description: "Showcasing innovative works from Semicolon Africa's technology labs.",
+    title: "Semicolon Labs -Coming Soon",
+    description: "Showcasing innovative works from Semicolon Africa's technology labs.",
+    icons: {
+        icon: "/Labs by Semicolon Primary Logo 1.svg",
+    },
 }
 export default function RootLayout({children,}: { children: React.ReactNode }) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
         <ThemeProvider>{children}</ThemeProvider>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
